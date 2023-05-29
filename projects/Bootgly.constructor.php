35c2a793d0116409fb4ae8f41964ba94c8d71537
Rodrigo Vieira <rodrigo@slayer.tech>
2023-05-29 18:14:45 -0300

    🌱 Added some resources folders

---
+++ projects/Bootgly.constructor.php
@@ -0,0 +1,13 @@
+<?php
+namespace projects;
+
+
+use Bootgly\CLI;
+use Bootgly\Web;
+
+
+if (\PHP_SAPI) {
+   $CLI = new CLI;
+} else {
+   $Web = new Web;
+}
---
+++ projects/CLI.constructor.php
@@ -0,0 +1,9 @@
+<?php
+namespace projects;
+
+
+use Bootgly\CLI;
+
+
+// CLI::$Commands
+// CLI::$Terminal
