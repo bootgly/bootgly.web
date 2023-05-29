516498120851177bd9b5090bbb2f68c6624f4e46
Rodrigo Vieira <rodrigo@slayer.tech>
2023-05-29 18:14:18 -0300

    🌱 Added some files

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * --------------------------------------------------------------------------
+ * Bootgly PHP Framework
+ * Developed by Rodrigo Vieira (@rodrigoslayertech)
+ * Copyright 2020-present
+ * Licensed under MIT
+ * --------------------------------------------------------------------------
+ */
+
+#error_reporting(E_ALL); ini_set('display_errors', 'On');
+
+#phpinfo(); exit;
+
+require_once '@imports/autoload.php';
