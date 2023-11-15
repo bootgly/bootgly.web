0d8e99eb4147b7196deadc75c93a28209a04e182
Rodrigo Vieira <rodrigo@slayer.tech>
2023-11-15 19:50:20 -0300

    🌱 Updated index.php file

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 define('BOOTGLY_WORKING_BASE', __DIR__);
 define('BOOTGLY_WORKING_DIR', BOOTGLY_WORKING_BASE . DIRECTORY_SEPARATOR);
 
-(@include __DIR__ . '/@imports/autoload.php')
-|| (@include __DIR__ . '/Bootgly/autoload.php')
-|| exit(1);
+// @ Autoload composer (if exists):
+(@include __DIR__ . '/@imports/autoload.php') ||
+(  // @ Autoload platforms (submodules):
+   (@include __DIR__ . '/Bootgly/autoload.php') &&
+   (@include __DIR__ . '/Console/autoload.php') &&
+   (@include __DIR__ . '/Web/autoload.php')
+) || exit(1);
