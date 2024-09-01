d4fa17b14befcf293ea084678c4c0d8662adf947
Rodrigo Vieira <rodrigo@slayer.tech>
2024-09-01 17:26:02 -0300

    🚚 ♻️ Use autoboot name to bootstrap files with spl_autoload_register

--- index.php
+++ index.php
@@ -13,8 +13,8 @@ define('BOOTGLY_WORKING_DIR', BOOTGLY_WORKING_BASE . DIRECTORY_SEPARATOR);
 
 // @ Autoload composer (if exists):
 (@include __DIR__ . '/@imports/autoload.php') ||
-(  // @ Autoload platforms (submodules):
-   (@include __DIR__ . '/Bootgly/autoload.php') &&
-   (@include __DIR__ . '/Console/autoload.php') &&
-   (@include __DIR__ . '/Web/autoload.php')
+(  // @ Autoboot platforms (submodules):
+   (@include __DIR__ . '/Bootgly/autoboot.php') &&
+   (@include __DIR__ . '/Console/autoboot.php') &&
+   (@include __DIR__ . '/Web/autoboot.php')
 ) || exit(1);
